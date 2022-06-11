ffb70fbc5a7db91a06ce2109a376f41e76695f0e
MacBook Air M1 <macbook@sskate.me>
2022-06-11 20:32:38 +0900

    feat: 2022-06-11 dump

---
+++ components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="bg-gray-900">
+    <div class="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex-1 md:flex md:items-center md:gap-12">
+          <a class="flex items-center text-white" href="/"
+            ><span class="sr-only">Home</span>
+            <img src="/logo.png" class="h-8 mr-2 rounded-full" loading="lazy" />
+            <span>Esoft, Korea</span></a
+          >
+        </div>
+        <div class="md:flex md:items-center md:gap-8">
+          <nav class="hidden md:block" aria-labelledby="header-navigation">
+            <h2 class="sr-only" id="header-navigation">Header navigation</h2>
+            <ul class="flex items-center gap-6 text-s">
+              <li>
+                <router-link
+                  class="text-white transition hover:text-white/75"
+                  to="/"
+                  >Home</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  class="text-white transition hover:text-white/75"
+                  to="/api"
+                  >API</router-link
+                >
+              </li>
+              <li>
+                <a
+                  class="text-white transition hover:text-white/75"
+                  href="https://forms.gle/s555uuasuLuSWydc6"
+                  >Recruit</a
+                >
+              </li>
+            </ul>
+          </nav>
+          <div class="flex items-center gap-4">
+            <div class="sm:gap-4 sm:flex">
+              <a
+                class="px-5 py-2.5 text-sm font-medium text-white bg-gray-600 rounded-md shadow"
+                href="/"
+                >Discord</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <!-- 내비게이션 바 -->
+    <Navbar />
+
+    <!-- 메인 헤더 -->
+    <section class="text-white bg-gray-900">
+      <div
+        class="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex"
+      >
+        <div class="max-w-3xl mx-auto text-center">
+          <h1
+            class="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 !leading-tight"
+          >
+            새로운 일상으로 도약하다.<span class="hidden sm:block"
+              >Esoft, Korea</span
+            >
+          </h1>
+          <p class="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+            illo tenetur fuga ducimus numquam ea!
+          </p>
+          <div class="flex flex-wrap justify-center gap-4 mt-8">
+            <a
+              class="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring"
+              href="/"
+              >Discord</a
+            ><a
+              class="block w-full px-12 py-3 text-sm font-medium text-white border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring"
+              href="https://forms.gle/s555uuasuLuSWydc6"
+              >Recruit</a
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- 팀원 -->
+    <section class="text-white bg-gray-900">
+      <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-lg mx-auto text-center">
+          <h2 class="text-3xl font-bold sm:text-4xl">Esoft, Korea 팀원</h2>
+          <p class="mt-4 text-gray-300">
+            저희 Esoft, Korea에서 일하시는 팀원을 확인하세요.
+          </p>
+        </div>
+
+        <div class="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3">
+          <div
+            class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-pink-500/10 hover:border-pink-500/10"
+            v-for="data in this.crew"
+            :key="data"
+          >
+            <img
+              class="w-10 h-10 rounded-full"
+              :src="
+                'https://cdn.discordapp.com/avatars/' +
+                data.id +
+                '/' +
+                data.avatar +
+                '.png'
+              "
+              alt="profile image"
+              loading="lazy"
+            />
+            <h3 class="mt-4 text-xl font-bold text-white">
+              {{ data.user_tag }}
+            </h3>
+            <p class="mt-1 text-sm text-gray-300">
+              {{ data.position }}
+            </p>
+          </div>
+        </div>
+
+        <div class="mt-12 text-center">
+          <a
+            class="inline-flex items-center px-8 py-3 mt-8 text-white bg-pink-600 border border-pink-600 rounded hover:bg-transparent active:text-pink-500 focus:outline-none focus:ring"
+            href="https://forms.gle/s555uuasuLuSWydc6"
+            ><span class="text-sm font-medium"> Recruit </span
+            ><svg
+              class="w-5 h-5 ml-3"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </section>
+    <div class="hidden md:flex w-full justify-center bg-gray-900">
+      <iframe
+        src="https://business.esoftkorea.co.kr/forms/"
+        style="border: 0; width: 900px; height: 130px"
+        scrolling="no"
+      ></iframe>
+    </div>
+
+    <!-- 푸터 -->
+    <Footer />
+  </div>
+</template>
+
+<script>
+import * as crew from '../static/crew.json'
+
+export default {
+  name: 'IndexPage',
+  data() {
+    return { crew: JSON.parse(JSON.stringify(crew)).default }
+  },
+}
+</script>
